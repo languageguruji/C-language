870dbe6580ba643c0cddf37ed6898b001ef0bd45
pransu <pransuinc@gmail.com>
2017-07-08 16:39:53 +0530

    Add print Two triangle using two loop.cpp

---
+++ print Two triangle using two loop.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,i,j;
+	char ch;
+	clrscr();
+	printf("How Many rows are there : ");
+	scanf("%d",&n);
+
+	for(i=n;i>=1;i--)
+	{
+		ch=64;
+		for(j=2*i;j>=1;j--)
+		{
+			if(j==i && j!=n)
+				printf("%*c",6*(n-j),' ');
+			else if(j>i)
+				ch=ch+1;
+			else if(j<i)
+				ch=ch+1;
+			printf("%3c",ch);
+		}
+		printf("\n");
+	}
+	getch();
+}
